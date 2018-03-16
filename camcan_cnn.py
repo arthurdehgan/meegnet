b54d797b6072f9b4103a194ee4737c5dea2a94c5
arthurdehgan <arthurdehgan@github.com>
2018-03-16 16:50:47 -0400

    update perbatch

--- camcan_cnn.py
+++ camcan_cnn.py
@@ -5,11 +5,14 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split as data_split
+from itertools import combinations
 from mne.io import read_raw_fif
-from params import DATA_PATH, SUBJECT_LIST, SAVE_PATH, CHAN_DF
+from params import DATA_PATH, SUBJECT_LIST, SAVE_PATH, CHAN_DF, LABELS, MODEL_PATH
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
+N_SUB_PER_BATCH = 10
 SAVE_PATH = SAVE_PATH
 
 
@@ -45,13 +48,13 @@ def load_subject(sub, data=None, timepoints=2000, ch_type='all'):
 def cnn_model_fn(features, labels, mode):
     """Model function for CNN."""
     # Input Layer
-    input_layer = tf.reshape(features["x"], [-1, features.shape[0],
-                                             features.shape[1], 1])
+    a, b, c = features['x'].shape
+    input_layer = tf.reshape(features["x"], [a, b, c, 1])
 
     # Convolutional Layer #1
     conv1 = tf.layers.conv2d(
         inputs=input_layer,
-        filters=5,
+        filters=4,
         kernel_size=[1, 5],
         padding="same",
         activation=tf.nn.relu)
@@ -60,11 +63,13 @@ def cnn_model_fn(features, labels, mode):
     pool1 = tf.layers.max_pooling2d(inputs=conv1,
                                     pool_size=[1, 5],
                                     strides=(1, 5))
-    print(pool1.shape)
+    # Dropout layer #1
+    dropout1 = tf.layers.dropout(
+        inputs=pool1, rate=0.25, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-    # Convolutional Layer #2 and Pooling Layer #2
+    # Convolutional Layer #2 and Pooling Layer #2 and Dropout layer #2
     conv2 = tf.layers.conv2d(
-        inputs=pool1,
+        inputs=dropout1,
         filters=16,
         kernel_size=[1, 5],
         padding="same",
@@ -72,11 +77,12 @@ def cnn_model_fn(features, labels, mode):
     pool2 = tf.layers.max_pooling2d(inputs=conv2,
                                     pool_size=[1, 5],
                                     strides=(1, 5))
-    print(pool2.shape)
+    dropout2 = tf.layers.dropout(
+        inputs=pool2, rate=0.25, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-    # Convolutional Layer #3 and Pooling Layer #3
+    # Convolutional Layer #3 and Pooling Layer #3 and Dropout layer #3
     conv3 = tf.layers.conv2d(
-        inputs=pool2,
+        inputs=dropout2,
         filters=32,
         kernel_size=[1, 5],
         padding="same",
@@ -84,19 +90,18 @@ def cnn_model_fn(features, labels, mode):
     pool3 = tf.layers.max_pooling2d(inputs=conv3,
                                     pool_size=[1, 5],
                                     strides=(1, 5))
-    print(pool3.shape)
+    dropout3 = tf.layers.dropout(
+        inputs=pool3, rate=0.25, training=mode == tf.estimator.ModeKeys.TRAIN)
     # Dense Layer
-    pool3_flat = tf.reshape(pool3, [-1, pool3.shape[1]
+    dropout3_flat = tf.reshape(dropout3, [-1, pool3.shape[1]
                                     * pool3.shape[2]
                                     * pool3.shape[3]])
-    print(pool3_flat.shape)
-    dense = tf.layers.dense(inputs=pool3_flat, units=306000,
+    layer_size = pool3_flat.shape[-1]
+    dense = tf.layers.dense(inputs=dropout3_flat, units=layer_size,
                             activation=tf.nn.relu)
-    dropout = tf.layers.dropout(
-        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
     # Logits Layer
-    logits = tf.layers.dense(inputs=dropout, units=2)
+    logits = tf.layers.dense(inputs=dense, units=2)
 
     predictions = {
         # Generate predictions (for PREDICT and EVAL mode)
@@ -129,12 +134,28 @@ def cnn_model_fn(features, labels, mode):
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
-def main(unused_argv):
-    train_sub_list = SUBJECT_LIST[:200]
-    test_sub_list = SUBJECT_LIST[500:]
+def eval(eval_sub_list):
     eval_data = None
-    train_data = None
+    eval_labels = []
+    for sub in test_sub_list:
+        if eval_data is not None:
+            eval_data, temp_labels = load_subject(sub, eval_data)
+            eval_labels += temp_labels
+        else:
+            eval_data, eval_labels = load_subject(sub)
+    eval_labels = np.array(eval_labels)
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False)
+    eval_results = camcan_classifier.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
+
+
+def train(train_sub_list):
     train_labels = []
+    train_data = None
     for sub in train_sub_list:
         if train_data is not None:
             train_data, temp_labels = load_subject(sub,
@@ -146,11 +167,11 @@ def main(unused_argv):
                                                     ch_type='mag')
     train_labels = np.array(train_labels)
     camcan_classifier = tf.estimator.Estimator(
-        model_fn=cnn_model_fn, model_dir="/home/kikuko/")
+        model_fn=cnn_model_fn, model_dir=MODEL_PATH)
 
     tensors_to_log = {"probabilities": "softmax_tensor"}
     logging_hook = tf.train.LoggingTensorHook(
-        tensors=tensors_to_log, every_n_iter=50)
+        tensors=tensors_to_log, every_n_iter=100)
 
     # Train the model
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -161,26 +182,24 @@ def main(unused_argv):
         shuffle=True)
     camcan_classifier.train(
         input_fn=train_input_fn,
-        steps=100000,
+        steps=10000,
         hooks=[logging_hook])
 
-    # Evaluate the model and print results
-    eval_labels = []
-    for sub in test_sub_list:
-        if eval_data is not None:
-            eval_data, temp_labels = load_subject(sub, eval_data)
-            eval_labels += temp_labels
-        else:
-            eval_data, eval_labels = load_subject(sub)
-        print(eval_data.shape, len(eval_labels))
-    eval_labels = np.array(eval_labels)
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": eval_data},
-        y=eval_labels,
-        num_epochs=1,
-        shuffle=False)
-    eval_results = camcan_classifier.evaluate(input_fn=eval_input_fn)
-    print(eval_results)
+
+def main(unused_argv):
+    X_train, X_test = data_split(SUBJECT_LIST,
+                                                  shuffle=True,
+                                                  stratify=LABELS,
+                                                  random_state=0,
+                                                  test_size=.1)
+    train_sub_list = combinations(X_train, N_SUB_PER_BATCH)
+    test_sub_list = combinations(X_test, N_SUB_PER_BATCH)
+
+    for sub_list in train_sub_list:
+        train(sub_list)
+
+    for sub_list in test_sub_list:
+        eval(sub_list)
 
 
 if __name__ == "__main__":
