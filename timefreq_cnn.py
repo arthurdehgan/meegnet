09e89cf4e85417b27ab295212fc8b114624fd49a
Arthur Dehgan <arthurdehgan@gmail.com>
2021-03-25 15:13:57 -0400

    added common parser

--- timefreq_cnn.py
+++ timefreq_cnn.py
@@ -1,7 +1,6 @@
 import os
 import gc
 import sys
-import argparse
 from itertools import product
 from time import time
 import torch
@@ -12,133 +11,14 @@ from scipy.io import savemat, loadmat
 from utils import nice_time as nt
 from params import TIME_TRIAL_LENGTH, NBINS
 from dataloaders import create_loaders
+import parser
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--lr",
-    type=float,
-    default=0.00001,
-    help="the starting learning rate of the optimizer",
-)
-parser.add_argument(
-    "--patience",
-    type=int,
-    default=20,
-    help="patience for early stopping",
-)
-parser.add_argument(
-    "--model-name",
-    type=str,
-    default="net",
-    help="Name of the network for file_save",
-)
-parser.add_argument(
-    "--num-workers",
-    type=int,
-    default=4,
-    help="number of workers to load data while gpu is processing",
-)
-parser.add_argument(
-    "--train_size",
-    type=float,
-    default=0.8,
-    help="The proportion of data to use in the train set",
-)
-parser.add_argument(
-    "--save",
-    type=str,
-    help="The path where the model will be saved.",
-)
-parser.add_argument(
-    "-p",
-    "--path",
-    type=str,
-    help="The path where the data samples can be found.",
-)
-parser.add_argument(
-    "--seed",
-    default=420,
-    type=int,
-    help="Seed to use for random splits.",
-)
-parser.add_argument(
-    "-s",
-    "--max-subj",
-    default=1000,
-    type=int,
-    help="maximum number of subjects to use (1000 uses all subjects)",
-)
-parser.add_argument(
-    "-e",
-    "--elec",
-    default="MAG",
-    choices=["GRAD", "MAG", "ALL"],
-    help="The type of electrodes to keep, default=MAG",
-)
-parser.add_argument(
-    "-b",
-    "--batch-size",
-    default=128,
-    type=int,
-    help="The batch size used for learning.",
-)
-parser.add_argument(
-    "-d",
-    "--dropout",
-    default=0.25,
-    type=float,
-    help="The dropout rate of the linear layers",
-)
-parser.add_argument(
-    "--times",
-    action="store_true",
-    help="Instead of running the training etc, run a series of test in order to choose best set of workers and batch sizes to get faster epochs.",
-)
-parser.add_argument(
-    "--log",
-    action="store_true",
-    help="stores all prints in a logfile instead of printing them",
-)
-parser.add_argument(
-    "--chunkload",
-    action="store_true",
-    help="Chunks the data and loads data batch per batch. Will be slower but is necessary when RAM size is too low to handle whole dataset.",
-)
-parser.add_argument(
-    "--debug",
-    action="store_true",
-    help="loads dummy data in the net to ensure everything is working fine",
-)
-parser.add_argument(
-    "--dropout_option",
-    default="same",
-    choices=["same", "double", "inverted"],
-    help="sets if the first dropout and the second are the same or if the first one or the second one should be bigger",
-)
-parser.add_argument(
-    "-l", "--linear", type=int, help="The size of the second linear layer"
-)
-parser.add_argument(
-    "-m",
-    "--mode",
-    type=str,
-    choices=["overwrite", "continue", "empty_run", "evaluate"],
-    default="continue",
-    help="Changes the mode the script is run for: overwrite will restart from scratch and overwrite any files with the same name; continue will load previous state and continue from last checkpoint; empty_run will run the training and testing without saving anything; evaluate will load the model to evaluate it on the test set.",
-)
 parser.add_argument(
     "-f",
     "--filters",
     type=tuple,
     default=(7, 7),
     help="The size of the first filters for convolution: tuple of the form (freq_filter (int), time_filter (int))",
-)
-parser.add_argument(
-    "-n",
-    "--nchan",
-    type=int,
-    default=102,
-    help="the number of channels for the first convolution, the other channel numbers scale with this one",
 )
 
 
@@ -151,8 +31,7 @@ def accuracy(y_pred, target):
 class Flatten(nn.Module):
     # Flatten layer used to connect between feature extraction and classif parts of a net.
     def forward(self, x):
-        x = x.view(x.size(0), -1)
-        return x
+        return x.reshape(x.size(0), -1)
 
 
 def load_checkpoint(filename):
@@ -366,6 +245,15 @@ class customNet(nn.Module):
         self.name = model_name
         print(model_name)
 
+        # self.freqnet = nn.Sequential(
+        #     *nn.ModuleList(
+        #         [
+        #             Flatten(),
+        #             nn.Dropout(dropout, inplace=True),
+        #         ]
+        #     )
+        # )
+
         self.freqnet = nn.Sequential(
             *nn.ModuleList(
                 [
@@ -385,7 +273,7 @@ class customNet(nn.Module):
                     nn.ReLU(),
                     nn.Conv2d(nchan, nchan, (1, timefilter)),
                     Flatten(),
-                    nn.Dropout(dropout),
+                    nn.Dropout(dropout, inplace=True),
                 ]
             )
         )
@@ -399,12 +287,20 @@ class customNet(nn.Module):
         return layers(torch.zeros((1, *input_size))).shape[-1]
 
     def forward(self, x):
-        time, freq = x[:, :, :, : self.nbins], x[:, :, :, self.nbins :]
-        a = self.freqnet(freq)
-        b = self.timenet(time)
-        x = torch.cat((Flatten()(a), Flatten()(b)), dim=1)
+        time, freq = x[:, :, :, : -self.nbins], x[:, :, :, -self.nbins :]
+        a = self.timenet(time)
+        b = self.freqnet(freq)
+        x = torch.cat((a, b), dim=1)
         out = self.classif(x)
         return out
+
+    # def forward(self, x):
+    #     time, freq = x[:, :, :, : self.nbins], x[:, :, :, self.nbins :]
+    #     a = self.freqnet(freq)
+    #     b = self.timenet(time)
+    #     x = torch.cat((Flatten()(a), Flatten()(b)), dim=1)
+    #     out = self.classif(x)
+    #     return out
 
     def save_model(self, filepath="."):
         if not filepath.endswith("/"):
