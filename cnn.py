b3180e33665477a4787da9a648f9835d8a6c841a
arthurdehgan <arthurdehgan@gmail.com>
2021-04-06 09:26:39 -0400

    removed encoding from cnn.py

--- cnn.py
+++ cnn.py
@@ -426,7 +426,6 @@ if __name__ == "__main__":
         logging.basicConfig(
             filename=save_path + model_name + ".log",
             filemode="a",
-            encoding="utf-8",
             level=logging.DEBUG,
             format="%(asctime)s %(message)s",
             datefmt="%m/%d/%Y %I:%M:%S %p",
