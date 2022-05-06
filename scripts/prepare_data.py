fd02693df87f74e1debf508845f1e3a9d2382418
Arthur Dehgan <arthurdehgan@gmail.com>
2022-05-06 11:37:11 -0400

    changed data path names for consistency and added option to prepare passive and smt data as well as prepare epoched data

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -5,25 +5,22 @@ The Camcan dataset is not open access and you need to send a request on the webs
 This script assumes a copy of the cc700 and dataman folders to a data path parsed through the argparser.
 
 TODO:
-    Maybe add a prompt on the amount of disk space and time required ? (later)
-    Crawl through all of the data with the option to select data_tupe (rest/passive/smt)
-        for smt and passive, cut in trials with labels and change sfreq
+    add a prompt on the amount of disk space and time required
 
 """
 import os
-import mne
 import logging
+import mne
 import pandas as pd
 import numpy as np
 from joblib import Parallel, delayed
-from copy import deepcopy
 from camcan.parsing import parser
 
 # "Arthur_Dehgan_1160"
 parser.add_argument(
     "--user",
     type=str,
-    help="name of the user folder for loading csv data",
+    help="name of the camcan user folder for loading csv data. Usually of the form forename_name_1234",
 )
 parser.add_argument(
     "--sfreq",
@@ -35,7 +32,7 @@ args = parser.parse_args()
 
 if args.log:
     logging.basicConfig(
-        filename=args.save + "prepare_data.log",
+        filename=args.save_path + "prepare_data.log",
         filemode="a",
         level=logging.INFO,
         format="%(asctime)s %(message)s",
@@ -49,17 +46,30 @@ else:
     )
 
 
-def prepare_data(sub, source_path, save_path, down_freq=200, user=args.user):
+def prepare_data(
+    sub,
+    source_path,
+    save_path,
+    camcan_user,
+    s_freq=200,
+    dattype="rest",
+    epoched=False,
+):
+    if dattype == "rest":
+        assert (
+            not epoched
+        ), "Can't load epoched resting state data as there are no events for it"
+    row = None
     data_path = os.path.join(
         source_path,
         "cc700/meg/pipeline/release005/BIDSsep/",
-        f"derivatives_{args.dattype}",
+        f"derivatives_{dattype}",
         "aa/AA_movecomp_transdef/aamod_meg_maxfilt_00003/",
     )
     csv_path = os.path.join(
         source_path,
         "dataman/useraccess/processed/",
-        user,
+        camcan_user,
         "standard_data.csv",
     )
     df = pd.read_csv(csv_path)
@@ -68,40 +78,66 @@ def prepare_data(sub, source_path, save_path, down_freq=200, user=args.user):
             fif_file = os.path.join(data_path, sub, file)
             break
 
-    data = mne.io.read_raw_fif(fif_file).resample(sfreq=down_freq)
-    bads = data.info["bads"]
+    raw = mne.io.read_raw_fif(fif_file).resample(sfreq=s_freq)
+    bads = raw.info["bads"]
     if bads == []:
         channels = []
         logging.info(sub)
-        logging.info(data.info)
-        channels.append(deepcopy(data).pick_types(meg="mag")[:][0])
-        channels.append(deepcopy(data).pick_types(meg="planar1")[:][0])
-        channels.append(data.pick_types(meg="planar2")[:][0])
+        logging.info(raw.info)
+        if epoched and dattype != "rest":  # dattype in ("passive","smt"):
+            events = mne.find_events(raw)
+            event_dict = {6: "auditory1", 7: "auditory2", 8: "auditory3", 9: "visual"}
+            data = mne.Epochs(raw, events, tmin=-0.15, tmax=0.65, preload=True)
+            labels = [event_dict[event] for event in events[:, -1]]
+            filename = f"{dattype}_{sub}_epoched.npy"
+        else:
+            data = raw
+            filename = f"{dattype}_{sub}.npy"
+        channels.append(data.get_data(picks="mag"))
+        channels.append(data.get_data(picks="planar1"))
+        channels.append(data.get_data(picks="planar2"))
         data = np.array(channels)
+        if dattype == "passive":
+            data = data.swapaxes(0, 1)
         logging.info(data.shape)
         save_path = os.path.join(save_path, f"downsampled_{args.sfreq}")
         if not os.path.exists(save_path):
             os.makedirs(save_path)
         sub = sub.split("-")[1]
-        np.save(os.path.join(save_path, f"{args.dattype}_{sub}.npy"), data)
+        np.save(os.path.join(save_path, filename), data)
         row = df[df["CCID"] == sub].values.tolist()[0]
-        return row
+        if dattype in ("passive", "smt"):
+            row.append(labels)
+        print(row)
+        print(data.shape)
     else:
         logging.info(f"{sub} was dropped because of bad channels {bads}")
-    return
+    return row
 
 
 if __name__ == "__main__":
-    """Main function."""
     data_path = os.path.join(
-        args.path,
+        args.data_path,
         "cc700/meg/pipeline/release005/BIDSsep/",
         f"derivatives_{args.dattype}",
         "aa/AA_movecomp_transdef/aamod_meg_maxfilt_00003/",
     )
     a = Parallel(n_jobs=6)(
-        delayed(prepare_data)(sub, args.path, args.save, args.sfreq)
+        delayed(prepare_data)(
+            sub,
+            args.data_path,
+            args.save_path,
+            camcan_user=args.user,
+            s_freq=args.sfreq,
+            epoched=args.epoched,
+            dattype=args.dattype,
+        )
         for sub in os.listdir(data_path)
     )
-    dataframe = pd.DataFrame(a, columns=["sub", "age", "sex", "hand", "Coil", "MT_TR"])
-    dataframe.to_csv(os.path.join(args.save, "participants_info.csv")
+    columns = ["sub", "age", "sex", "hand", "Coil", "MT_TR"]
+    if args.dattype != "rest":
+        columns.append("event_labels")
+    dataframe = pd.DataFrame(a, columns=columns)
+    dataframe.to_csv(
+        os.path.join(args.save_path, f"participants_info_{args.dattype}.csv")
+    )
