ee1163b1cacd681f03975e83d0e3924092ae1942
Arthur <arthurdehgan@hotmail.com>
2024-01-22 19:07:35 -0500

    changed package name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="camcan",
+    name="meegnet",
     packages=find_packages(),
     version="0.1.0",
-    description="an ensemble of classification tools for camcan dataset classification",
+    description="A set of MEG-optimized ANNs and visualization tools.",
     author="Arthur Dehgan",
     license="MIT",
 )
