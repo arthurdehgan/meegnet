9d83244ef8f5cc9daa16786024f67c00e3ddf469
Arthur Dehgan <arthurdehgan@gmail.com>
2024-06-19 11:00:08 -0400

    added napoleon extension for numpy style doc

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ release = '0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_rtd_theme", "sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.napoleon", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
