c4b54f5efbabdf6c86cd8c7dbf506ab70f1f2742
Arthur Dehgan <arthurdehgan@gmail.com>
2024-06-19 11:07:11 -0400

    fixing up doc

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import find_packages, setup
-import sphinx_rtd_theme
 
 setup(
     name="meegnet",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.1.1",
     description="A set of MEG-optimized ANNs and visualization tools.",
     author="Arthur Dehgan",
     license="MIT",
