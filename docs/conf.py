812f8879d9a4889343d2b02d413abdc622d3e80f
Arthur <arthurdehgan@hotmail.com>
2024-07-23 15:36:11 -0400

    nbsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ release = "0.1.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme", "sphinx.ext.napoleon"]
+extensions = ["nbsphinx", "sphinx.ext.autodoc", "sphinx_rtd_theme", "sphinx.ext.napoleon"]
 
 # generate autosummary pages
 autosummary_generate = True
