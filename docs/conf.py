b26cd4758134932d6a7d801ef14fd5eec32a450a
Arthur <arthurdehgan@hotmail.com>
2024-10-24 09:27:07 -0400

    version 0.2.0 published

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../meegnet/"))
 project = "MEEGNet"
 copyright = "2024, Arthur Dehgan"
 author = "Arthur Dehgan"
-release = "0.1.1"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="meegnet",
     packages=find_packages(),
-    version="0.1.1",
+    version="0.2.0",
     description="A set of MEG-optimized ANNs and visualization tools.",
     author="Arthur Dehgan",
     license="MIT",
