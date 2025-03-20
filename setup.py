8dae694e3e47b89b7a58e3c27c5cdc8df0f85836
arthurdehgan <arthurdehgan@hotmail.com>
2025-03-20 00:54:18 -0400

    adding new setup for new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="meegnet",
     packages=find_packages(),
-    version="0.2.3",
+    version="0.2.4",
     description="A set of MEG-optimized ANNs and visualization tools.",
     author="Arthur Dehgan",
     license="MIT",
